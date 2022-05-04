f44989dd4c234d5431ed862e25a72cff7317e98b
DamsDev <loupegame@gmail.com>
2022-05-04 14:18:53 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# WulinGate
+
+[WulinGate](https://wulingate.com/en/): local and internet file sharing in your browser. Developed based on [Snapdrop](https://github.com/RobinLinus/snapdrop)
+
+## Main difference from original Snapdrop
+* Using a 4-digits number as device name instead of two English words.
+* Has internet transfer capability, which based on a 6-digits number room system. 
+* The [website](https://www.wulingate.com/en/) using my own turn server instead of a public stun server, thus guaranteed transfer success rate in internet transfer situation and in complex network environment (public wifi, company network, etc.).
+* Has a received messages list, which can show last 20 received messages. 
+
+
+#### WulinGate is built with the following awesome technologies:
+* Vanilla HTML5 / ES6 / CSS3 frontend
+* [WebRTC](http://webrtc.org/) / [WebSockets](http://www.websocket.org/)
+* [NodeJS](https://nodejs.org/en/) backend
+* [Progressive Web App](https://de.wikipedia.org/wiki/Progressive_Web_App)
+
+
+Have any questions? Read our [FAQ](/docs/faq.md).
+
+You can [host your own instance with Docker](/docs/local-dev.md).
+
---
+++ server/index.js
@@ -0,0 +1,246 @@
+var process = require('process')
+// Handle SIGINT
+process.on('SIGINT', () => {
+  console.info("SIGINT Received, exiting...")
+  process.exit(0)
+})
+
+// Handle SIGTERM
+process.on('SIGTERM', () => {
+  console.info("SIGTERM Received, exiting...")
+  process.exit(0)
+})
+
+const parser = require('ua-parser-js');
+
+class SnapdropServer {
+
+    constructor(port) {
+        const WebSocket = require('ws');
+        this._wss = new WebSocket.Server({ port: port });
+        this._wss.on('connection', (socket, request) => this._onConnection(new Peer(socket, request)));
+
+        this._rooms = {};
+
+        console.log('Snapdrop is running on port', port);
+    }
+
+    _onConnection(peer) {
+        this._joinRoom(peer);
+        peer.socket.on('message', message => this._onMessage(peer, message));
+        this._keepAlive(peer);
+
+        // send displayName
+        this._send(peer, {
+            type: 'display-name',
+            message: {
+                displayName: peer.name.displayName,
+                deviceName: peer.name.deviceName,
+                room: peer.room
+            }
+        });
+    }
+
+    _onMessage(sender, message) {
+        // Try to parse message 
+        try {
+            message = JSON.parse(message);
+        } catch (e) {
+            return; // TODO: handle malformed JSON
+        }
+
+        switch (message.type) {
+            case 'disconnect':
+                this._leaveRoom(sender);
+                break;
+            case 'pong':
+                sender.lastBeat = Date.now();
+                break;
+        }
+
+        // relay message to recipient
+        if (message.to && this._rooms[sender.ip]) {
+            const recipientId = message.to; // TODO: sanitize
+            const recipient = this._rooms[sender.ip][recipientId];
+            delete message.to;
+            // add sender id
+            message.sender = sender.id;
+            this._send(recipient, message);
+            return;
+        }
+    }
+
+    _joinRoom(peer) {
+        // if room doesn't exist, create it
+        if (!this._rooms[peer.ip]) {
+            this._rooms[peer.ip] = {};
+        }
+
+        // notify all other peers
+        for (const otherPeerId in this._rooms[peer.ip]) {
+            const otherPeer = this._rooms[peer.ip][otherPeerId];
+            this._send(otherPeer, {
+                type: 'peer-joined',
+                peer: peer.getInfo()
+            });
+        }
+
+        // notify peer about the other peers
+        const otherPeers = [];
+        for (const otherPeerId in this._rooms[peer.ip]) {
+            otherPeers.push(this._rooms[peer.ip][otherPeerId].getInfo());
+        }
+
+        this._send(peer, {
+            type: 'peers',
+            peers: otherPeers
+        });
+
+        // add peer to room
+        this._rooms[peer.ip][peer.id] = peer;
+    }
+
+    _leaveRoom(peer) {
+        if (!this._rooms[peer.ip] || !this._rooms[peer.ip][peer.id]) return;
+        this._cancelKeepAlive(this._rooms[peer.ip][peer.id]);
+
+        // delete the peer
+        delete this._rooms[peer.ip][peer.id];
+
+        peer.socket.terminate();
+        //if room is empty, delete the room
+        if (!Object.keys(this._rooms[peer.ip]).length) {
+            delete this._rooms[peer.ip];
+        } else {
+            // notify all other peers
+            for (const otherPeerId in this._rooms[peer.ip]) {
+                const otherPeer = this._rooms[peer.ip][otherPeerId];
+                this._send(otherPeer, { type: 'peer-left', peerId: peer.id });
+            }
+        }
+    }
+
+    _send(peer, message) {
+        if (!peer) return;
+        if (this._wss.readyState !== this._wss.OPEN) return;
+        message = JSON.stringify(message);
+        peer.socket.send(message, error => '');
+    }
+
+    _keepAlive(peer) {
+        this._cancelKeepAlive(peer);
+        var timeout = 30000;
+        if (!peer.lastBeat) {
+            peer.lastBeat = Date.now();
+        }
+        if (Date.now() - peer.lastBeat > 2 * timeout) {
+            this._leaveRoom(peer);
+            return;
+        }
+
+        this._send(peer, { type: 'ping' });
+
+        peer.timerId = setTimeout(() => this._keepAlive(peer), timeout);
+    }
+
+    _cancelKeepAlive(peer) {
+        if (peer && peer.timerId) {
+            clearTimeout(peer.timerId);
+        }
+    }
+}
+
+class Peer {
+
+    constructor(socket, request) {
+        // set socket
+        this.socket = socket;
+
+        // set peer id, code, room
+        this._setPeerValues(request);
+
+        // set remote ip
+        this._setIP(request);
+
+        // is WebRTC supported ?
+        this.rtcSupported = request.url.indexOf('webrtc') > -1;
+        // set name 
+        this._setName(request);
+        // for keepalive
+        this.timerId = 0;
+        this.lastBeat = Date.now();
+    }
+
+    _setPeerValues(request) {
+        let params = (new URL(request.url, "http://server")).searchParams;
+        this.id = params.get("peerid");
+        this.code = params.get("code");
+        let incomeRoomId = params.get("roomid").replace(/\D/g,'');
+        if (incomeRoomId.length == 6) {
+            this.room = incomeRoomId;
+        }else {
+            this.room = '';
+        }
+    }
+
+    _setIP(request) {
+        if (this.room){
+            this.ip = this.room;
+        }else if (request.headers['x-forwarded-for']) {
+            this.ip = request.headers['x-forwarded-for'].split(/\s*,\s*/)[0];
+        }else {
+            this.ip = request.connection.remoteAddress;
+        }
+        // IPv4 and IPv6 use different values to refer to localhost
+        if (this.ip == '::1' || this.ip == '::ffff:127.0.0.1') {
+            this.ip = '127.0.0.1';
+        }
+    }
+
+    toString() {
+        return `<Peer id=${this.id} ip=${this.ip} rtcSupported=${this.rtcSupported}>`
+    }
+
+    _setName(req) {
+        let ua = parser(req.headers['user-agent']);
+
+
+        let deviceName = '';
+        
+        if (ua.os && ua.os.name) {
+            deviceName = ua.os.name.replace('Mac OS', 'Mac') + ' ';
+        }
+        
+        if (ua.device.model) {
+            deviceName += ua.device.model;
+        } else {
+            deviceName += ua.browser.name;
+        }
+
+        if(!deviceName)
+            deviceName = 'Unknown';
+
+        let displayName = this.code;
+
+        this.name = {
+            model: ua.device.model,
+            os: ua.os.name,
+            browser: ua.browser.name,
+            type: ua.device.type,
+            deviceName,
+            displayName
+        };
+    }
+
+    getInfo() {
+        return {
+            id: this.id,
+            name: this.name,
+            room: this.room,
+            rtcSupported: this.rtcSupported
+        }
+    }
+
+}
+
+const server = new SnapdropServer(process.env.PORT || 3000);
